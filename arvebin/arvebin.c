4f4c0011a11139f1597dfb2a2bef9a53b11b309f
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2020-03-17 20:04:21 -0300

    grrrrr

--- arvebin/arvebin.c
+++ arvebin/arvebin.c
@@ -9,20 +9,6 @@ typedef struct nodo{
     struct nodo *pai;
 } nodo;
 
-
-nodo *binary(nodo *n, int chave){
-    if (n == NULL)
-        return criaNodo(chave);
-
-    else if (n->chave < chave)
-        return binary(n->dir, chave);
-    else if (n->chave < chave)
-        return binary(n->esq, chave);
-
-    return n;
-}
-
-
 void inOrder(nodo *n){
     if (n != NULL){
         inOrder(n->esq);
@@ -31,16 +17,73 @@ void inOrder(nodo *n){
     }
 }
 
+nodo *maximo(nodo *x){
+	while(x->dir != NULL)
+		x = x->dir;
+	return x;
+}
+
+nodo *minimo(nodo *x){
+	while(x->esq != NULL)
+		x = x->esq;
+	return x;
+}
+
+nodo *buscaBin(nodo *x, int k){
+	if (x == NULL || x->chave == k)
+		return x;
+
+	if (k < x->chave)
+		return buscaBin(x->esq, k);
+	return buscaBin(x->dir, k);	
+
+}
+
+nodo *inserir(nodo *raiz, nodo *z){
+	
+	nodo *y, *x;
+	y = NULL;
+	x = raiz;
+
+	while(x != NULL){
+		printf("z = %d -- x = %d\n", z->chave, x->chave);	
+		y = x;
+		if (z->chave < x->chave)
+			x = x->esq;
+		else
+			x = x->dir;
+	}
+	z->pai = y;
+	if (z->chave < y->chave)
+		y->esq = y;
+	else 
+		y->dir = z;
+	return z;
+}
+
+nodo *criaNodo(int k){
+	nodo *n = malloc(sizeof(nodo));
+	n->dir = NULL;
+	n->esq = NULL;
+	n->pai = NULL;
+	n->chave = k;
+	return n;
+}
+
 int main(){
 
     srand(time(NULL));
 
     nodo *raiz = criaNodo(50);
 
-    for (int i = 0; i < 10; i++)
-        binary(raiz, rand()%100);
+	nodo *n = criaNodo(20);
+	inserir(raiz, n);
+	for (int i = 0; i < 10; i++){
+	}
 
-    inOrder(raiz);
-
+    //inOrder(raiz);
+	nodo *m = minimo(raiz);
+	printf("minimo = %d\n", m->chave);
+	
     return 0;
 }
